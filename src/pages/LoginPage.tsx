a4fb6eafd15d1cad7aafd6a0a37e7af6b5573d72
Goodness <idowugudness@gmail.com>
2025-05-10 08:30:11 +0100

    Add login and signup form functionality

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { LoginForm } from '../components/auth/LoginForm';
+
+
+const LoginPage: React.FC = () => {
+    return (
+        <div className='m-10'>
+        <LoginForm />
+        </div>
+    );
+};
+
+export default LoginPage;
