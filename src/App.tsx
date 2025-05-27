b6c94338023d39bbb4a921da3ede31232e75b1de
Goodness <idowugudness@gmail.com>
2025-05-27 19:19:24 +0100

    modify main.tsx to display app tag

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
 import LoginPage2 from './pages/LoginPage2'; // Import the new LoginPage2 component
+import Dashboard from './pages/Dashboard'; // Import Dashboard if needed
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/sign-up" element={<SignUpPage />} />
         <Route path="/login2" element={<LoginPage2 />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         {/* <Route path="/sign-up" element={<SignUpPage />} /> */}
         {/* Add other routes here */}
         {/* Add / login, /register and /dashboard routes later */}
--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 //  import { jsxDEV } from "react/jsx-dev-runtime"; 
 import './index.css'
-import Dashboard from './pages/Dashboard.tsx'
+import App from './App'
 
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-  {/* { <App />}  */}
-    <Dashboard />
+    <App />
   </StrictMode>,
 )
