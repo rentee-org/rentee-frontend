87cee4af60dd6168bb4305090ec92f202427e629
Goodness <idowugudness@gmail.com>
2025-05-30 22:02:16 +0100

    Added files to target-folder/ in main-app-setup

---
+++ src/components/constants/index.js
@@ -0,0 +1,29 @@
+import user from "../../assets/images/Rentee-user.png"
+
+
+ const UserProfile = [
+    {
+        id: 1,
+        name: "onyekachi",
+        img: user,
+    },
+    // {
+    //     id: 2,
+    //     name: "jennifer",
+    //     img: user,
+    // }
+    // {
+    //     id: 1,
+    //     name: "John",
+    //     img: user,
+    // },
+    // {
+    //     id: 1,
+    //     name: "onyekachi",
+    //     img: user,
+    // },
+
+];
+
+export default UserProfile;
+
